567b6a4ca6266a7a3f716773811a0938e2761231
meydson <64757536+meydson@users.noreply.github.com>
2020-06-06 10:27:43 -0300

    configurando node.js

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const server = express()
+server.use(express.static("public"))
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+server.listen(3000)
+
+/*
+Comandos Node.js
+-->Startar servidor
+    npm start server
+-->Instalar template engine
+    npm install nunjucks
+*/
