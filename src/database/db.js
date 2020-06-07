88ee8d085a0d7547366fb682dff7ab0704f1207f
meydson <64757536+meydson@users.noreply.github.com>
2020-06-07 02:00:42 -0300

    projeto finalizado

--- src/database/db.js
+++ src/database/db.js
@@ -1,58 +1,59 @@
 const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database("./src/database/database.db") 
-/*
-db.serialize( () => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT, 
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
-    const query = `
-        INSERT INTO places (
-            image,
-            name,
-            address,
-            address2,
-            state,
-            city,
-            items
-        ) VALUES (?,?,?,?,?,?,?);
-    `
-    const values = [
-        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        "Papperside",
-        "Guilherme Gambala, Jardim América",
-        "Número 260",
-        "Santa Catarina",
-        "Rio do Sul",
-        "Papéis e papelão" 
-    ]
-    function afterInsertData (err) {
-        if (err) {
-            console.log(err)
-        }
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
-    db.run(query, values, afterInsertData)
-   db.all(`SELECT * FROM places`, function(err, rows) {
-        if (err) {
-            console.log(err)
-        }
-        console.log("resultados")
-        console.log(rows)
-   })
-   db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
-        if (err) {
-            console.log(err)
-        }
-        console.log("Registro deletado com sucesso!")
-   })*/
-})
+module.exports = db
+
+// db.serialize( () => {
+// //     db.run(`
+// //         CREATE TABLE IF NOT EXISTS places (
+// //             id INTEGER PRIMARY KEY AUTOINCREMENT,
+// //             image TEXT,
+// //             name TEXT,
+// //             address TEXT, 
+// //             address2 TEXT,
+// //             state TEXT,
+// //             city TEXT,
+// //             items TEXT
+// //         );
+// //     `)
+// //     const query = `
+// //         INSERT INTO places (
+// //             image,
+// //             name,
+// //             address,
+// //             address2,
+// //             state,
+// //             city,
+// //             items
+// //         ) VALUES (?,?,?,?,?,?,?);
+// //     `
+// //     const values = [
+// //         "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+// //         "Papperside",
+// //         "Guilherme Gambala, Jardim América",
+// //         "Número 260",
+// //         "Santa Catarina",
+// //         "Rio do Sul",
+// //         "Papéis e papelão" 
+// //     ]
+// //     function afterInsertData (err) {
+// //         if (err) {
+// //             console.log(err)
+// //         }
+// //         console.log("Cadastrado com sucesso")
+// //         console.log(this)
+// //     }
+// //     db.run(query, values, afterInsertData)
+// //    db.all(`SELECT * FROM places`, function(err, rows) {
+// //         if (err) {
+// //             console.log(err)
+// //         }
+// //         console.log("resultados")
+// //         console.log(rows)
+// //    })
+// //    db.run(`DELETE FROM places WHERE id = ?`, [3], function(err) {
+// //         if (err) {
+// //             console.log(err)
+// //         }
+// //         console.log("Registro deletado com sucesso!")
+// //    })
+// })
